9fa1597218e4b7be167cb8744ae10763daccf484
rex <rex@remind101.com>
2023-07-02 19:15:38 -0700

    fix supported mac os version

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "AutoGraphParser",
     platforms: [
         .iOS(.v13),
-        .macOS(.v10_15),
+        .macOS(.v10_13),
         .tvOS(.v13),
         .watchOS(.v6),
     ],
